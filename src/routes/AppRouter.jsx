6cc0080bcc938f0207bbdac8d351f3a77d23ef31
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2026-07-26 13:45:04 +0530

    update Approuter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,8 @@
 // src/routes/AppRoutes.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast'; // <--- ME IMPORT EKA ADD KARANNA
+
 import { InventoryProvider } from '../context/InventoryContext';
 import MainLayout from '../layouts/MainLayout';
 import DashboardPage from '../pages/DashboardPage';
@@ -10,6 +12,20 @@ import HistoryPage from '../pages/HistoryPage';
 const AppRoutes = () => {
   return (
     <InventoryProvider>
+      {/* Toast Alert Config */}
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#1f2937',
+            color: '#fff',
+            borderRadius: '12px',
+            fontSize: '13px',
+          },
+        }}
+      />
+
       <Router>
         <Routes>
           <Route path="/" element={<MainLayout />}>
